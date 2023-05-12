41eddc9efc73525ed7ba87110011cc39a3c20336
Giusier <giusier@hotmail.com>
2023-05-12 15:24:55 -0300

    base for api

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: "vars.env" });
+
+// dependencys
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// routes
+const routes = require("./routes.js");
+
+// server
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.listen(process.env.API_PORT, () => {
+  console.log(`\n\n\nServidor rodando em: http://localhost:${process.env.API_PORT}\n\n\n`);
+});
